e6797ff55cf40a959ad3fbeee7a856298aff2853
Troy <troy.lamerton@gmail.com>
2017-03-30 19:43:47 +0200

    Create react app for proto1

---
+++ proto1/src/components/Player/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'style.css';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      picId: 'newbie',
+      picSrc: '',
+    };
+  }
+
+  render() {
+    <div>
+  }
+}
